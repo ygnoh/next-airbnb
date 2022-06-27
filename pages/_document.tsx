001c5359e0773d97859a2104a38fa5149f75b2de
yonggoo-noh <yonggoo.noh@navercorp.com>
2022-06-27 11:21:06 +0900

    Set font family

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Document, { DocumentContext } from "next/document";
+import Document, { DocumentContext, Html, Main, Head, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
@@ -24,5 +24,26 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css?family=Noto+Sans:400,700&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css?family=Noto+Sans+KR:400,700&display=swap&subset=korean"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
